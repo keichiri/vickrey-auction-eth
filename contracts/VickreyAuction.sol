944a81390e34f37c868c5a4e861fd50bab86ae09
Janko Krstic <keichiri@protonmail.com>
2018-11-11 19:28:00 +0100

    core biding and revealing

--- contracts/VickreyAuction.sol
+++ contracts/VickreyAuction.sol
@@ -4,6 +4,16 @@ pragma solidity 0.4.24;
 /// @title Contract implementng VickreyAuction on Ethereum
 ///        The item that is being sold is represented by a opaque string
 ///        which should be hash of the item's digital version.
+///        If there is only single revealer, price is set to his bid.
+///        This encourages confident bidders to reveal very late.
+///        NOTE - in order to prevent non-reveals additional fee can be added
+///        NOTE - does not include paying currently. That is for future
+///        iteration. Paying can be done in 2 ways:
+///          1. everyone pays same deposit when bidding. If later full payment
+///             by the winner is not provided, his deposit is taken and given to other
+///             participants or owner
+///          2. everyone says money when making bids that should be equal or greater
+///             than the actual bid (to hide the actual value)
 /// @author Janko Krstic <keichiri@protonmail.com>
 contract VickreyAuction {
     address public owner;
@@ -11,19 +21,21 @@ contract VickreyAuction {
     uint public revealStart;
     uint public revealFinish;
     uint public price;
+    address public winner;
     mapping (address => Bid) bids;
     uint public bidCount;
-    Bid public winningBid;
+    Bid winningBid;
 
     struct Bid {
         bytes32 hash;
-        int value;
+        uint value;
     }
 
     event NewBid(address bidder);
+    event BidReveal(address bidder, uint value);
 
 
-    constructor(string _item, uint _revealStart, uint _revealFinish) {
+    constructor(string _item, uint _revealStart, uint _revealFinish) public {
         require(_revealStart > now, "Reveal start must be set in the future");
         require(_revealFinish > _revealStart, "Reveal finish must be set after revealStart");
         require(bytes(_item).length > 0, "Item must not be empty string");
@@ -34,7 +46,57 @@ contract VickreyAuction {
         item = _item;
     }
 
+    function bid(bytes32 hash) public {
+        require(now < revealStart, "Bids cannot be made after the reveal phase is started");
+        // Imagine if this is actually valid hash, 1 / 2**256
+        require(hash != bytes32(0), "Hash cannot be zero");
+
+        Bid storage unrevealedBid = bids[msg.sender];
+        if (unrevealedBid.hash == bytes32(0)) {
+            bidCount++;
+        }
+
+        bids[msg.sender].hash = hash;
+        emit NewBid(msg.sender);
+    }
+
+    function reveal(uint value, bytes32 nonce) public {
+        require(now > revealStart && now < revealFinish, "Reveal can be made only during reveal phase");
+
+        Bid storage unrevealedBid = bids[msg.sender];
+        require(unrevealedBid.hash != bytes32(0), "Did not bid");
+        require(unrevealedBid.value == 0, "Already revealed");
+
+        bytes memory input = abi.encodePacked(value, nonce);
+        bytes32 hash = sha256(input);
+        require(hash == unrevealedBid.hash, "Revealed data not correct");
+
+        unrevealedBid.value = value;
+        emit BidReveal(msg.sender, value);
+
+        // Setting the largest so far
+        if (unrevealedBid.value > winningBid.value) {
+            if (price != 0) {
+                price = winningBid.value;
+            } else {
+                price = unrevealedBid.value;
+            }
+
+            winningBid = unrevealedBid;
+            winner = msg.sender;
+        }
+    }
+
     function isOpen() public view returns (bool) {
         return now < revealStart;
+    }
+
+    function getWinningBid() public view returns (bytes32, uint) {
+        return (winningBid.hash, winningBid.value);
+    }
+
+    // Only for easier testing
+    function createHash(uint value, bytes32 nonce) public pure returns (bytes32) {
+        return sha256(abi.encodePacked(value, nonce));
     }
 }
--- test/vickreyAuction.js
+++ test/vickreyAuction.js
@@ -1,4 +1,5 @@
 const VickreyAuction = artifacts.require("./VickreyAuction.sol");
+const crypto = require('crypto');
 
 
 async function assertRevert(promise) {
@@ -11,6 +12,21 @@ async function assertRevert(promise) {
   }
 }
 
+async function retrieveEvent(tx, eventName) {
+  const { logs } = await tx;
+  return logs.find(e => e.event === eventName);
+}
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function sha256(input) {
+  let hasher = crypto.createHash('sha256');
+  hasher.update(input, 'binary');
+  return hasher.digest('binary');
+}
 
 contract("VickreyAuction", accounts => {
   let auction;
@@ -23,13 +39,18 @@ contract("VickreyAuction", accounts => {
   let now;
   let goodRevealStartTime;
   let goodRevealFinishTime;
+  let nonce1;
+  let bid1 = 1000;
+  let hash1;
 
 
   beforeEach(async() => {
     now = (new Date()).getTime() / 1000;
     goodRevealStartTime = now + 2;
     goodRevealFinishTime = now + 4;
+    nonce1 = sha256(Math.floor(Math.random() * 100000).toString()); // for simplicity
     auction = await VickreyAuction.new(item, goodRevealStartTime, goodRevealFinishTime);
+    hash1 = await auction.createHash(bid1, nonce1);
     assert.ok(auction);
   });
 
@@ -54,4 +75,115 @@ contract("VickreyAuction", accounts => {
       assert.equal(price, 0);
     });
   });
+
+  describe("Bidding", () => {
+    it("Works if input is correct", async() => {
+      await auction.bid(hash1, {from: bidder1});
+
+      let bidCount = await auction.bidCount();
+
+      assert.equal(bidCount, 1);
+    });
+
+    it("Allows to bid 2 times", async() => {
+      await auction.bid(hash1, {from: bidder1});
+      await auction.bid(hash1, {from: bidder1});
+
+      let bidCount = await auction.bidCount();
+
+      assert.equal(bidCount, 1);
+    });
+
+    it("Reverts if called after reveal starts", async() => {
+      await sleep(2000);
+
+      await assertRevert(auction.bid(hash1, {from: bidder1}));
+    });
+  });
+
+  describe("Revealing", () => {
+    beforeEach(async() => {
+      await auction.bid(hash1, {from: bidder1});
+    });
+
+    it("Sets the largest", async() => {
+      await sleep(3000); // TODO - find a better way for this
+      await auction.reveal(bid1, nonce1, {from: bidder1});
+      let winningBid = await auction.getWinningBid();
+      let price = await auction.price();
+      let winner = await auction.winner();
+
+      assert.equal(winningBid[0], hash1);
+      assert.equal(winningBid[1], bid1);
+      assert.equal(winner, bidder1);
+      assert.equal(price, bid1);
+    });
+
+    it("Emits BidReveal event", async() => {
+      await sleep(3000);
+      let tx = await auction.reveal(bid1, nonce1, {from: bidder1});
+      let event = await retrieveEvent(tx, "BidReveal");
+
+      assert.equal(event.args.bidder, bidder1);
+      assert.equal(event.args.value, bid1);
+    });
+
+    it("Sets winning bid and price correctly if multiple bids", async() => {
+      let bid2 = 2000;
+      let nonce2 = sha256(Math.floor(Math.random() * 100000).toString()); // for simplicity
+      let hash2 = await auction.createHash(bid2, nonce2);
+      await auction.bid(hash2, {from: bidder2});
+
+      let bid3 = 500;
+      let nonce3 = sha256(Math.floor(Math.random() * 1000000).toString());
+      let hash3 = await auction.createHash(bid3, nonce3);
+      await auction.bid(hash3, {from: bidder3});
+
+      await sleep(3000);
+
+      await auction.reveal(bid1, nonce1, {from: bidder1});
+      await auction.reveal(bid2, nonce2, {from: bidder2});
+      await auction.reveal(bid3, nonce3, {from: bidder3});
+
+      let winningBid = await auction.getWinningBid();
+      let price = await auction.price();
+      let winner = await auction.winner();
+
+      assert.equal(winningBid[0], hash2);
+      assert.equal(winningBid[1], bid2);
+      assert.equal(price, bid1);
+      assert.equal(winner, bidder2);
+    });
+
+    it("Reverts if wrong value", async() => {
+      await sleep(3000);
+      await assertRevert(auction.reveal(bid1 + 1, nonce1, {from: bidder1}));
+    });
+
+    it("Reverts if wrong nonce", async() => {
+      await sleep(3000);
+      await assertRevert(auction.reveal(bid1, sha256("some data"), {from: bidder1}));
+    });
+
+    it("Reverts if revealing not open", async() => {
+      await assertRevert(auction.reveal(bid1, nonce1, {from: bidder1}));
+    });
+
+    it("Reverts if revealing finished", async() => {
+      await sleep(5000);
+      await assertRevert(auction.reveal(bid1, nonce1, {from: bidder1}));
+    });
+
+    it("Reverts if did not bid", async() => {
+      await sleep(3000);
+      await assertRevert(auction.reveal(bid1, nonce1, {from: nonBidder}));
+    });
+
+    it("Reverts if already revealed", async() => {
+      await sleep(3000);
+      await auction.reveal(bid1, nonce1, {from: bidder1});
+      await assertRevert(auction.reveal(bid1, nonce1, {from: bidder1}));
+    });
+  });
+
 });
